305cb549ca2743438ef9c80ef1124abe88bf1532
Ben Magriso <benmagriso1@gmail.com>
2018-05-04 16:37:07 +0300

    garage

---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Collections.Generic;
+
+public class Garage
+{
+    private List<GarageVechile> m_GarageVechiles;
+
+    private GarageVechile getVechileByLicenceNumber(string i_LicenceNumber)
+    {
+        GarageVechile garageVechileToReturn =null;
+        foreach (GarageVechile garageVechile in m_GarageVechiles)
+        {
+            if (garageVechile.ownerVechile.licenceNumber == i_LicenceNumber)
+            {
+                garageVechileToReturn = garageVechile;
+            }
+        }
+        return garageVechileToReturn;
+    }
+    public string addVechileToGarage(Vechile i_VechileToAdd,string i_OwnerName,string i_PhoneOfOwner)
+    {
+        string message=null;
+        GarageVechile newGarageVechileToAdd = new GarageVechile();
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_VechileToAdd.licenceNumber);
+        if(garageVechileInList != null)
+        {
+            message = "The Vechile is already in the garage";
+            garageVechileInList.currentVechileStatus = Repair;
+        }
+       else
+        {
+            newGarageVechileToAdd.ownerVechile = i_VechileToAdd;
+            newGarageVechileToAdd.ownerName = i_OwnerName;
+            newGarageVechileToAdd.phoneOfOwner = i_PhoneOfOwner;
+            newGarageVechileToAdd.currentVechileStatus = Repair;
+            m_GarageVechiles.Add(newGarageVechileToAdd);
+            message = "The Vechile added to the garage";
+        }
+
+        return message;
+    }
+    public List<string> showVechilesInGarage(eVechileStatus? i_statusToFilterVechiles)
+    {
+        List<string> vechilesLicenceNumbers = new List<string>();
+        foreach (GarageVechile garageVechile in m_GarageVechiles)
+        {
+            if(i_statusToFilterVechiles == null)
+            {
+                vechilesLicenceNumbers.Add(garageVechile.ownerVechile.licenceNumber);
+            }
+
+            if (garageVechile.currentVechileStatus == i_statusToFilterVechiles)
+            {
+                vechilesLicenceNumbers.Add(garageVechile.ownerVechile.licenceNumber);
+            }
+                
+        }
+
+        return vechilesLicenceNumbers;
+    }
+    public void changeStatusOfVechile(string i_LicenceNumber, eVechileStatus i_NewStatus)
+    {
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_LicenceNumber);
+        if (garageVechileInList != null)
+        {
+            garageVechileInList.currentVechileStatus = i_NewStatus;
+        }
+    }
+    public void inflateAirInTheWheels(Vechile i_VechileToAddPressure)
+    {
+        float currentAirPressure, maxAirPressure,missingAirPressure;
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_VechileToAddPressure.licenceNumber);
+        if (garageVechileInList != null)
+        {
+            currentAirPressure = i_VechileToAddPressure.Wheels.currentAirPressure;
+            maxAirPressure = i_VechileToAddPressure.Wheels.maxAirPressure;
+            missingAirPressure = maxAirPressure - currentAirPressure;
+            i_VechileToAddPressure.Wheels.addAirPressure(missingAirPressure);
+        }
+    
+        
+    }
+    public void putInFuelInGasolineEngine(string i_LicenceNumber,eTypeOfFuel i_TypeOfFuelToAdd,float,i_AmountOfFuelToAdd)
+    {
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_LicenceNumber);
+        if (garageVechileInList != null)
+        {
+            garageVechileInList.engineOfVechile.FillInTheContainer(i_AmountOfFuelToAdd); // check it!!!!!!!!!
+        }
+    }
+    public void chargeElectricEngine(string i_LicenceNumber,float i_AmountOfMinToAdd)
+    {
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_LicenceNumber);
+        if (garageVechileInList != null)
+        {
+            garageVechileInList.engineOfVechile.FillInTheContainer(i_AmountOfMinToAdd);  // check it!!!!!!!!!
+        }
+    }
+    public string getCarDetails(string i_LicenceNumber)
+    {
+        string carDetails=null;
+        GarageVechile garageVechileInList = new GarageVechile();
+        garageVechileInList = getVechileByLicenceNumber(i_LicenceNumber);
+        if (garageVechileInList != null)
+        {
+            carDetails = string.Format("The Licence number is {0}, The Model Name is {1}, The Owner Name is {2}\n The type in the garage is {3}, The Wheel Pressure is {4}");
+        }
+
+
+            return carDetails;
+    }
+}
+
---
+++ Ex03.GarageLogic/eVechileStatus.cs
@@ -0,0 +1,7 @@
+﻿
+public enum eVechileStatus
+{
+    Repair,
+    Fixed,
+    Paid
+}
